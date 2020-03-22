6fb3dad76231a5beefe245dd8398c6f1113f2bb1
changyuyun <969842624@qq.com>
2020-03-22 13:07:35 +0800

    修复登陆小案例示例代码bug

--- src/cn/ityun/web/servlet/LoginServlet.java
+++ src/cn/ityun/web/servlet/LoginServlet.java
@@ -2,6 +2,7 @@ package cn.ityun.web.servlet;
 
 import cn.ityun.web.dao.UserDao;
 import cn.ityun.web.domain.User;
+import org.apache.commons.beanutils.BeanUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +10,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
 @WebServlet(urlPatterns = "/loginS")
 public class LoginServlet extends HttpServlet {
     @Override
@@ -19,14 +23,31 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("utf-8");
-        String name = req.getParameter("username");
-        String pwd = req.getParameter("password");
+        //1.手动获取方式
+        /*String name = req.getParameter("name");
+        String pwd = req.getParameter("pwd");
         User loginUser = new User();
         loginUser.setName(name);
-        loginUser.setPwd(pwd);
+        loginUser.setPwd(pwd);*/
+        //2.beanutils获取方式
+        //说明：JavaBean:标准的java类
+        //类必须被public修饰
+        //必须提供空参的构造器
+        //成员变量必须使用private修饰
+        //提供公共getter & setter
+        Map<String, String[]> parameterMap = req.getParameterMap();
+        User loginUser = new User();
+        try {
+            BeanUtils.populate(loginUser, parameterMap);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
         UserDao userDao = new UserDao();
         User user = userDao.login(loginUser);
-        resp.getWriter().println(name +"-----"+ pwd);
+        // resp.getWriter().println(name +"-----"+ pwd);
         resp.getWriter().println("user");
         if (user == null) {
             //resp.getWriter().println("failed");
